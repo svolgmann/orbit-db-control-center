e8199ebfca310533e0ac6dcb2956c47961054b18
soeren <soeren-volgmann@web.de>
2021-07-28 14:04:07 +0200

    some custom settings

--- src/config/index.js
+++ src/config/index.js
@@ -6,9 +6,8 @@ const config = {
     config: {
       Addresses: {
         Swarm: [
-          '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-          '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-          '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/'
+          '/dns4/signal.gordic5.com/tcp/443/wss/p2p-webrtc-star/',
+          '/dns6/signal.gordic5.com/tcp/443/wss/p2p-webrtc-star/'
         ]
       }
     }
